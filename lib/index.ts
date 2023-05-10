a10d6f700682fa42f2d7f12ce108150889c6339d
Konstantinos Liberopoulos <konstantinos.liberopoulos@causaly.com>
2023-05-10 17:00:11 +0300

    refactor: export type to `NonNullish`

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,7 @@
 export { isNotNull } from './isNotNull';
 export { isNotUndefined } from './isNotUndefined';
 export { isNotNullish } from './isNotNullish';
-export type { NotNullish } from './isNotNullish';
+export type { NonNullish } from './isNotNullish';
 
 export { isNotEmptyString } from './isNotEmptyString';
 
--- lib/isNotNullish.ts
+++ lib/isNotNullish.ts
@@ -1,5 +1,5 @@
-export type NotNullish<T> = Exclude<T, null | undefined>;
+export type NonNullish<T> = Exclude<T, null | undefined>;
 
-export function isNotNullish<T>(value: T): value is NotNullish<T> {
+export function isNotNullish<T>(value: T): value is NonNullish<T> {
   return value != null;
 }
